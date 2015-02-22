73f6ca113d7c9010636de728cb04e9219f1ec23f
RRisto <ristohinno@gmail.com>
2015-02-22 21:00:21 +0200

    submission

---
+++ palgakalkulaatori proov.R
@@ -0,0 +1,117 @@
+tm.vaba=154
+tm.maar=0.2
+sm.maar=0.33
+kp.maar=0.02
+tk.maar.tootaja=0.016
+tk.maar.tooandja=0.008
+palk.bruto=1000
+
+#arvutus
+sm=palk.bruto*sm.maar
+tk.tootaja=palk.bruto*tk.maar.tootaja
+tk.tooandja=palk.bruto*tk.maar.tooandja
+kp=palk.bruto*kp.maar
+tm=(palk.bruto-tm.vaba-tk.tootaja-kp)*tm.maar
+
+palk.neto=palk.bruto-tk.tootaja-kp-tm
+
+tooandja.kulu=palk.bruto+sm+tk.tooandja
+maksuametile.koik=sm+tk.tootaja+tk.tooandja+tm+kp
+tulem=c(palk.neto, tooandja.kulu, maksuametile.koik, tm, sm,kp,
+        tk.tootaja, tk.tooandja)
+tulem.names=c("Netopalk", "Tööandja kulu", "Maksuametile kantav summa", "Tulumaks",
+              "Sotsiaalmaks", "Kogumispension", "Töötuskindlustus (töötaja)",
+              "Töötuskindlustus(tööandja)")
+tulem.kokku=data.frame(tulem.names, tulem)
+names(tulem.kokku)=c(" ", "Summa (€)")
+
+netopalk <- function(palk.bruto, tm.vaba=154, tm.maar=0.2, sm.maar=0.33, 
+                      kp.maar=0.02, tk.maar.tootaja=0.016, tk.maar.tooandja=0.008){
+    #arvutused
+    sm=palk.bruto*sm.maar
+    tk.tootaja=palk.bruto*tk.maar.tootaja
+    tk.tooandja=palk.bruto*tk.maar.tooandja
+    kp=palk.bruto*kp.maar
+    tm=(palk.bruto-tm.vaba-tk.tootaja-kp)*tm.maar
+    #palk
+    palk.neto=palk.bruto-tk.tootaja-kp-tm
+    #kulu eri osapooltele
+    tooandja.kulu=palk.bruto+sm+tk.tooandja
+    maksuametile.koik= sm+tk.tootaja+tk.tooandja+tm+kp
+    #tulemite koondamine
+    tulem=c(palk.bruto,palk.neto, tooandja.kulu, maksuametile.koik, tm, sm,kp,
+            tk.tootaja, tk.tooandja)
+    tulem.names=c("Brutopalk", "Netopalk", "Tööandja kulu", 
+                  "Maksuametile kantav summa", "Tulumaks","Sotsiaalmaks", 
+                  "Kogumispension", "Töötuskindlustus (töötaja)",
+                  "Töötuskindlustus(tööandja)")
+    tulem.kokku=data.frame(tulem.names, tulem)
+    names(tulem.kokku)=c(" ", "Summa (€)")
+    #tulemi esitamine
+    print(tulem.kokku, row.names=F)
+}
+
+netopalk(1000)
+
+#kalkulaator brutopalga leidmiseks
+tm.vaba=154
+tm.maar=0.2
+sm.maar=0.33
+kp.maar=0.02
+tk.maar.tootaja=0.016
+tk.maar.tooandja=0.008
+palk.neto=1000
+
+#arvutus
+palk.bruto=round((palk.neto-tm.vaba*tm.maar)/
+                     (1-tk.maar.tootaja-kp.maar-tm.maar+
+                          tk.maar.tootaja*tm.maar+kp.maar*tm.maar),2)
+sm=round(palk.bruto*sm.maar, 2)
+tk.tootaja=round(palk.bruto*tk.maar.tootaja, 2)
+tk.tooandja=round(palk.bruto*tk.maar.tooandja, 2)
+kp=round(palk.bruto*kp.maar, 2)
+tm=round((palk.bruto-tm.vaba-tk.tootaja-kp)*tm.maar, 2)
+
+tooandja.kulu=palk.bruto+sm+tk.tooandja
+maksuametile.koik=sm+tk.tootaja+tk.tooandja+tm+kp
+tulem=c(palk.bruto, palk.neto, tooandja.kulu, maksuametile.koik, tm, sm,kp,
+        tk.tootaja, tk.tooandja)
+tulem.names=c("Brutopalk", "Netopalk", "Tööandja kulu", "Maksuametile kantav summa",
+              "Tulumaks","Sotsiaalmaks", "Kogumispension", 
+              "Töötuskindlustus (töötaja)","Töötuskindlustus(tööandja)")
+tulem.kokku=data.frame(tulem.names, tulem)
+names(tulem.kokku)=c(" ", "Summa (€)")
+
+brutopalk <- function(palk.neto, tm.vaba=154, tm.maar=0.2, sm.maar=0.33, 
+                     kp.maar=0.02, tk.maar.tootaja=0.016, tk.maar.tooandja=0.008){
+    #arvutused
+    palk.bruto=round((palk.neto-tm.vaba*tm.maar)/
+                         (1-tk.maar.tootaja-kp.maar-tm.maar+
+                              tk.maar.tootaja*tm.maar+kp.maar*tm.maar),2)
+    sm=round(palk.bruto*sm.maar, 2)
+    tk.tootaja=round(palk.bruto*tk.maar.tootaja, 2)
+    tk.tooandja=round(palk.bruto*tk.maar.tooandja, 2)
+    kp=round(palk.bruto*kp.maar, 2)
+    tm=round((palk.bruto-tm.vaba-tk.tootaja-kp)*tm.maar, 2)
+    #kulu eri osapooltele
+    tooandja.kulu=palk.bruto+sm+tk.tooandja
+    maksuametile.koik=sm+tk.tootaja+tk.tooandja+tm+kp
+    #tulemite koondamine
+    tulem=c(palk.bruto, palk.neto, tooandja.kulu, maksuametile.koik, tm, sm,kp,
+            tk.tootaja, tk.tooandja)
+    tulem.names=c("Brutopalk", "Netopalk", "Tööandja kulu", "Maksuametile kantav summa", "Tulumaks",
+                  "Sotsiaalmaks", "Kogumispension", "Töötuskindlustus (töötaja)",
+                  "Töötuskindlustus (tööandja)")
+    tulem.kokku=data.frame(tulem.names, tulem)
+    names(tulem.kokku)=c(" ", "Summa (€)")
+    #tulemi esitamine
+    print(tulem.kokku, row.names=F)
+}
+
+brutopalk(1000)
+#proovime ka nullmääradega
+brutopalk(1000, tm.vaba=0, tm.maar=0.2, sm.maar=0.33, 
+kp.maar=0, tk.maar.tootaja=0.016, tk.maar.tooandja=0.008)
+
+
+plot(brutopalk(1000))
